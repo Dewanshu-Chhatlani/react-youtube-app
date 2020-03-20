8f9caab03fb82d300e181f6683359bc7a0d3044b
Dewanshu-Chhatlani <dewanshu.chhatlani@diaspark.com>
2020-03-20 15:49:22 +0530

    Created VideoItem Component and rendered a list of VideoItems.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
 class VideoList extends React.Component {
   render() {
-    return <div>{this.props.videos.length}</div>;
+    const renderedList = this.props.videos.map(video => {
+      return <VideoItem video={video} />;
+    });
+    return <div>{renderedList}</div>;
   }
 }
 
